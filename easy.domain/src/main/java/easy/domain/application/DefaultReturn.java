45e338be8c4d550523a33b2f903f32a958fd3f2a
lixiaojing <lixiaojing@etaostars.com>
2016-08-09 22:18:01 +0800

    优化代码

--- easy.domain/src/main/java/easy/domain/application/DefaultReturn.java
+++ easy.domain/src/main/java/easy/domain/application/DefaultReturn.java
@@ -7,7 +7,6 @@ public class DefaultReturn<T> implements IReturn {
 	private T obj;
 	private List<IReturnTransformer> transformer;
 	private static final NotFoundReturnTransformer noFound = new NotFoundReturnTransformer();
-
 	public DefaultReturn(T obj, List<IReturnTransformer> transformers) {
 		this.obj = obj;
 		this.transformer = transformers;
@@ -25,5 +24,10 @@ public class DefaultReturn<T> implements IReturn {
 		}
 		
 		return noFound.getValue(context, obj);
+	}
+
+	@Override
+	public Object resultDefault() throws Exception {
+		return this.obj;
 	}
 }
--- easy.domain/src/main/java/easy/domain/application/IReturn.java
+++ easy.domain/src/main/java/easy/domain/application/IReturn.java
@@ -2,4 +2,5 @@ package easy.domain.application;
 
 public interface IReturn {
 	Object result(ReturnContext context) throws Exception;
+	Object resultDefault() throws Exception;
 }
