0b5f44ceb416d1e488d2233bcb5a5039271aed34
lixiaojing <lee.bmw@foxmail.com>
2016-10-09 11:27:21 +0800

    去掉多余的代码

--- easy.domain/src/test/java/easy/xml/test/XmlTest.java
+++ easy.domain/src/test/java/easy/xml/test/XmlTest.java
@@ -2,12 +2,9 @@ package easy.xml.test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
