cffa5616fa3096f3a63df07d73ddb79513d7c254
lixiaojing <lixiaojing3@jd.com>
2017-02-20 17:17:01 +0800

    增加创建业务规则异常业务方法

--- src/main/java/easy/domain/base/BrokenRuleMessage.java
+++ src/main/java/easy/domain/base/BrokenRuleMessage.java
@@ -4,25 +4,29 @@ import java.util.HashMap;
 
 public abstract class BrokenRuleMessage {
 
-	private HashMap<String, String> messages;
+    private HashMap<String, String> messages;
 
-	public BrokenRuleMessage() {
-		this.messages = new HashMap<String, String>();
-		this.populateMessage();
-	}
+    public BrokenRuleMessage() {
+        this.messages = new HashMap<String, String>();
+        this.populateMessage();
+    }
 
-	protected HashMap<String, String> getMessages() {
-		return this.messages;
-	}
+    protected HashMap<String, String> getMessages() {
+        return this.messages;
+    }
 
-	protected abstract void populateMessage();
+    protected abstract void populateMessage();
 
-	public String getRuleDescription(String messageKey) {
+    public String getRuleDescription(String messageKey) {
 
-		String desc = "";
-		if (this.messages.containsKey(messageKey)) {
-			desc = this.messages.get(messageKey);
-		}
-		return desc;
-	}
+        String desc = "";
+        if (this.messages.containsKey(messageKey)) {
+            desc = this.messages.get(messageKey);
+        }
+        return desc;
+    }
+
+    public BrokenRuleException createException(String messageKey) {
+        return new BrokenRuleException(messageKey, this.getRuleDescription(messageKey));
+    }
 }
