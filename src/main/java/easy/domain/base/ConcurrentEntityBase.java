2ad692cd5d576c6c49a4eb49f5aa9198e9ca03be
lixiaojing <lixiaojing3@jd.com>
2017-09-07 11:26:53 +0800

    增加并发实体

---
+++ src/main/java/easy/domain/base/ConcurrentEntityBase.java
@@ -0,0 +1,26 @@
+package easy.domain.base;
+
+import java.io.Serializable;
+
+public abstract class ConcurrentEntityBase<T extends Serializable> extends EntityBase<T> {
+    private long oldVersion;
+
+    public long getOldVersion() {
+        return oldVersion;
+    }
+
+    protected void setOldVersion(long oldVersion) {
+        this.oldVersion = oldVersion;
+    }
+
+    public long getNewVersion() {
+        return ++this.oldVersion;
+    }
+
+
+    @Override
+    protected abstract BrokenRuleMessage getBrokenRuleMessages();
+
+    @Override
+    public abstract Boolean validate();
+}
